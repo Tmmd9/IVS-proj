ff6ec14457a4b5a0de786b3fd1060c4b41c78348
Tmmd9 <xkovac49@stud.fit.vutbr.cz>
2021-03-29 15:57:05 +0200

    profiling

---
+++ src/profile.py
@@ -0,0 +1,41 @@
+from mathlib import *
+from fileinput import input
+
+
+def mean_calc(nums):
+    sum = 0
+    for num in nums:
+        sum = add(int(num),sum)
+
+    mean = div(sum,len(nums))
+
+    return mean
+
+
+def variance(data):
+    n = len(data)
+    mean = mean_calc(data)
+    deviations = [pow((sub(float(x),mean)),2) for x in data]
+    deviations_sum=0
+    for dev in deviations:
+        deviations_sum = add(deviations_sum,dev)
+    variance = deviations_sum / n
+    return variance
+
+def stdev(data):
+  var = variance(data)
+  std_dev = root(var, 2)
+  return std_dev
+
+
+def main():
+    num_array = input()
+    final_array = list()
+    for number in num_array:
+        final_array.append(number)
+
+    final=final_array[0].split(" ")
+    print("Mean of numbers: "+str(mean_calc(final)))
+    print("Standard deviation is: "+str(stdev(final)))
+
+main()
